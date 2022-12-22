9a49d1c21af3beca444703f869061211bda0fed4
technical_aks <105633032+technicalaks@users.noreply.github.com>
2022-12-22 14:06:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -134,7 +134,8 @@ async def message_handler(event):
         )
         message = f'**ᴄʟɪᴄᴋ ʜᴇʀᴇ 👇🏻 ꜰᴏʀ "{event.text}"**\n\n[♻️📽️ {str(event.text).upper()}\n📝🔎 {str("click here for result").upper()}]({tgraph_result})'
 
-        newbutton = [Button.url('Join Updates Channel', 'https://t.me/Imdb_updates')]
+        newbutton = [Button.url('ᴊᴏɪɴ ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ',
+                                    f'http:/t.me/Imdb_updates')]
 
         await txt.delete()
         result = await event.reply(message, link_preview=False)
