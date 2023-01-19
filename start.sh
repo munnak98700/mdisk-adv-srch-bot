f205117806d7f06f55d01ff75db900ea02c5eec8
munnak98700 <122736995+munnak98700@users.noreply.github.com>
2023-01-19 20:31:27 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/munnak98700/mdisk-adv-srch-bot/MdiskWalaBot
+git clone https://github.com/munnak98700/Link-Search-Bot-Pro /MdiskWalaBot
 cd /MdiskWalaBot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
